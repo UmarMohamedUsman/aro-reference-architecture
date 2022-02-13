38da08985f9c08a7e304d29aa43ca99a00a54aa3
Umar Mohamed Usman <umarm@microsoft.com>
2022-02-13 13:12:40 -0800

    Changes based on recent testing

--- deployment/script.ps1
+++ deployment/script.ps1
@@ -7,26 +7,28 @@ Remove-Item -Path .\AzureCLI.msi
 
 
 #############################
-#Install Docker -
+#Install Docker - https://docs.docker.com/desktop/windows/install/#install-docker-desktop-on-windows
 # Need to figure out how to do this in quiet mode
 #############################
-Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Install-Package -Name docker -ProviderName DockerMsftProvider
-#  A restart is required to enable the containers feature. *** Restart the machine***
-Start-Service Docker
+# Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
+# Install-Package -Name docker -ProviderName DockerMsftProvider
+# #  A restart is required to enable the containers feature. *** Restart the machine***
+# Start-Service Docker
 
 #############################
-#Install Kubectl
+# Install Kubectl -  https://kubernetes.io/docs/tasks/tools/install-kubectl-windows/
+# BTW, Docker Desktop adds its own version of kubectl to PATH as well
 #############################
-curl -LO "https://dl.k8s.io/release/v1.23.0/bin/windows/amd64/kubectl.exe"
-Install-Script -Name 'install-kubectl' -Scope CurrentUser -Force
-install-kubectl.ps1 -DownloadLocation ./
+# curl -LO "https://dl.k8s.io/release/v1.23.0/bin/windows/amd64/kubectl.exe"
+# Install-Script -Name 'install-kubectl' -Scope CurrentUser -Force
+# install-kubectl.ps1 -DownloadLocation ./
 
 
 #############################
-#Install Helm
+#Install Helm : https://helm.sh/docs/intro/install/
 #############################
 
+
 #############################
-#Install OC CLI
+#Install OC CLI : https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-windows.zip
 #############################
